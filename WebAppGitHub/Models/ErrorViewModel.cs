cc39f1b883c7fa6936c59d52fcd95d83be1935e7
Khaja Moinuddin <KhajaM@fixitytech.com>
2022-11-21 19:20:07 +0530

    Add project files.

---
+++ WebAppGitHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppGitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
