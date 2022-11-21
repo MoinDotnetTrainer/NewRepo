6a4fca99f4f8b9fd655af95541c16589a9f2b685
Khaja Moinuddin <KhajaM@fixitytech.com>
2022-11-21 20:17:03 +0530

    comment

---
+++ WebAppGitHub/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppGitHub.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
